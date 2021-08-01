96eaa63e7d7876231bb60fd1ff0b9f0fca7f1f47
JungHyun0902 <kimjh5354@gmail.com>
2021-08-01 22:38:41 +0900

    Add Inheritance (Extension)

--- main.js
+++ main.js
@@ -32,3 +32,35 @@ console.log(amy.getFullName())
 console.log(neo.getFullName()) 
 
 ////////////////////////////////////
+
+class Vehicle {
+  constructor(name, wheel) {
+    this.name = name
+    this.wheel = wheel
+  }
+}
+const myVehicle = new Vehicle('운송수단', 2)
+console.log(myVehicle)
+
+class Bicycle extends Vehicle {
+  constructor(name, wheel) {
+    super(name, wheel)
+  }
+}
+const myBicycle = new Bicycle('삼천리', 2)
+const daughtersBicycle = new Bicycle('세발', 3)
+console.log(myBicycle)
+console.log(daughtersBicycle)
+
+class Car extends Vehicle {
+  constructor(name, wheel, license) {
+    super(name, wheel)
+    this.license = license
+  }
+}
+const myCar = new Car('애스턴마틴 발할라', 4, true)     
+const daughtersCar = new Car('모닝', 4, true)
+console.log(myCar)
+console.log(daughtersCar)
+
+//////////////////////////////////////////////////////////////
